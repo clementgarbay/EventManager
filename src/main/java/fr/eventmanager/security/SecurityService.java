3c0440eef1e26419a2be4e858f06a59901becc92
Clément Garbay <clementgarbay@gmail.com>
2016-10-22 15:04:04 +0200

    Remove useless code

--- src/main/java/fr/eventmanager/security/SecurityService.java
+++ src/main/java/fr/eventmanager/security/SecurityService.java
@@ -40,33 +40,4 @@ public class SecurityService {
         if (session != null) session.invalidate();
     }
 
-
-    //    public boolean isLoggedIn(HttpServletRequest request) {
-//        HttpSession session = request.getSession();
-//
-//        boolean isLoggedIn = false;
-//        if(session.getAttribute(SECURITY_IS_LOGGED) == null) {
-//            session.setAttribute(SECURITY_IS_LOGGED, false);
-//        } else {
-//            isLoggedIn = (boolean) session.getAttribute(SECURITY_IS_LOGGED);
-//        }
-//
-//        return isLoggedIn && userConnected.isRegistered();
-//    }
-//
-//    public void setLogged(HttpServletRequest request, User user) {
-//        HttpSession session = request.getSession();
-//
-//        session.setAttribute(SECURITY_IS_LOGGED, true);
-//        userConnected = user;
-//    }
-//
-//    public void clear(HttpServletRequest request) {
-//        setLogged(request, false, null);
-//    }
-//
-//    public User getLoggedUser() {
-//        return userConnected;
-//    }
-
 }
